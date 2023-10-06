313d4eeacd857fa91f0e7d9c09712f7cf2c509ce
p4sscode <p4ssenger.developer@gmail.com>
2023-10-05 23:15:27 -0300

    move hdc and graph code into different files

--- papers/hdc.py
+++ papers/hdc.py
@@ -2,7 +2,6 @@
 
 import numpy as np
 import functools as ft
-import networkx as nx
 
 
 # Basic arithemitic operations
@@ -45,29 +44,3 @@ class ItemMemory:
 
     def cleanup(self, V):
         return max(self.vectors, key=lambda x: cosine_similarity(V, x[1]))
-
-
-# Graph pre-processing
-
-def processDataset(dataset):
-    graphs = []
-    labels = []
-
-    for graph in dataset:
-        G = nx.Graph()
-        G.add_edges_from(zip(graph["edge_index"][0], graph["edge_index"][1]))
-        graphs.append(G)
-        labels.append(graph["y"][0])
-
-    return (graphs, labels)
-
-def transform(X, alpha, digits):
-    graphs = []
-    for graph in X:
-        gpr = nx.pagerank(graph, alpha)
-        nodes = dict()
-        for key, value in gpr.items():
-            nodes[key] = str(round(value, digits))
-        H = nx.relabel_nodes(graph, nodes)
-        graphs.append(H)
-    return graphs
