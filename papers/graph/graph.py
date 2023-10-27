fd1abc694dc57794e4d4efaa2236ce12f5538e28
p4sscode <p4ssenger.developer@gmail.com>
2023-10-27 13:47:23 -0300

    more tests with different centrality ranks

--- papers/graph/graph.py
+++ papers/graph/graph.py
@@ -4,6 +4,7 @@ import numpy as np
 import functools as ft
 import networkx as nx
 
+from memory_profiler import profile
 
 # Graph pre-processing
 
@@ -26,9 +27,6 @@ def transform(X, alpha, digits):
     graphs = []
     for graph in X:
         gpr = nx.pagerank(graph, alpha)
-        # ecr = nx.eigenvector_centrality_numpy(graph)
-        # kcr = nx.katz_centrality_numpy(graph, alpha)
-        # dcr = nx.degree_centrality(graph)
         nodes = dict()
         for key, value in gpr.items():
             nodes[key] = str(round(value, digits))
@@ -38,15 +36,5 @@ def transform(X, alpha, digits):
 
 
 def centrality(X, rank):
-    graphs = []
     for graph in X:
-        cr = rank(graph)
-        H = nx.relabel_nodes(graph, cr)
-        graphs.append(H)
-    return graphs
-    #     nodes = dict()
-    #     for key, value in cr.items():
-    #         nodes[key] = str(round(value, digits))
-    #     H = nx.relabel_nodes(graph, nodes)
-    #     graphs.append(H)
-    # return graphs
+        yield nx.relabel_nodes(graph, rank(graph))
